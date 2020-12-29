cc7a12a0c743073c326f1de62522267c9f92a3c7
draganjancikin <dragan.jancikin@gmail.com>
2020-12-29 13:12:47 +0100

    index.php update

--- web/index.php
+++ web/index.php
@@ -1,7 +1,20 @@
 <?php
+
+require __DIR__ . '/../vendor/autoload.php';
+use Roloffice\Core\App;
+App::run();
+
+/*
+// old index bafore App::run();
 require __DIR__ . '/../vendor/autoload.php';
 
 use Roloffice\Core\Config;
+use Roloffice\Core\Logger;
 
 $LOG_PATH = Config::get('LOG_PATH', '');
 echo "[LOG_PATH]: $LOG_PATH";
+
+Logger::enableSystemLogs();
+$logger = Logger::getInstance();
+$logger->info('Hello World');
+*/
