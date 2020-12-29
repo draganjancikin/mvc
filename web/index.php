8f903518e6ea01caf1e28d0e0827537db0dde257
draganjancikin <dragan.jancikin@gmail.com>
2020-12-29 13:21:51 +0100

    index update

--- web/index.php
+++ web/index.php
@@ -1,7 +1,22 @@
 <?php
-
 require __DIR__ . '/../vendor/autoload.php';
+
 use Roloffice\Core\App;
+use Roloffice\Core\Router;
+use Roloffice\Core\Request;
+use Roloffice\Core\Response;
+
+Router::get('/', function () {
+    echo 'Hello World';
+});
+
+Router::get('/post/([0-9]*)', function (Request $req, Response $res) {
+    $res->toJSON([
+        'post' =>  ['id' => $req->params[0]],
+        'status' => 'ok'
+    ]);
+});
+
 App::run();
 
 /*
